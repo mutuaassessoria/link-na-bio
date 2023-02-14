2ed1a88e8dd08cd6a20850767b072c37c9d14a99
henriqueteixeiradev <henriqueteixeira.dev@gmail.com>
2023-02-14 10:18:03 -0300

    ajuste

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/global.styles";
 import theme from "styles/theme";
 import Head from "next/head";
-import Script from "next/script";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,40 +14,29 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/icon-512.png" />
         <link rel="apple-touch-icon" href="/icon-512.png" />
       </Head>
-      {/* <Script
-        id="my-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${process.env.GTM}');`,
-        }}
-      ></Script> */}
-      {/* <NextSeo
-        title="IEDI"
-        description="Conquiste seu certificado do Ensino Médio com as facilidades do EAD em no máximo 18 meses"
-        canonical="https://eja.iedicursos.net.br/"
+      <NextSeo
+        title="Mútua Assessoria"
+        description="Nosso propósito é transformar sua empresa em um negócio de sucesso."
+        canonical="https://bio.mutuaassessoria.com"
         openGraph={{
-          url: "https://eja.iedicursos.net.br/",
-          title: "IEDI",
+          url: "https://bio.mutuaassessoria.com",
+          title: "Mútua Assessoria",
           description:
-            "Conquiste seu certificado do Ensino Médio com as facilidades do EAD em no máximo 18 meses",
+            "Nosso propósito é transformar sua empresa em um negócio de sucesso.",
           images: [
             {
               url: "/cover.png",
             },
           ],
-          site_name: "IEDI",
+          site_name: "Mútua Assessoria",
           locale: "pt_BR",
         }}
         twitter={{
-          handle: "@iedicursos",
+          handle: "@mutua.assessoria",
           site: "@site",
           cardType: "summary_large_image",
         }}
-      /> */}
+      />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
